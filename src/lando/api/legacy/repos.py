a2a51f5becb2ad8312a582b0fe4f74455a11c06e
Zeid Zabaneh <zeid@mozilla.com>
2024-01-26 15:31:20 -0500

    ui: port revisions page and supporting functionality (bug 1876838)
    WIP DO NOT MERGE

--- src/lando/api/legacy/repos.py
+++ src/lando/api/legacy/repos.py
@@ -14,7 +14,7 @@ from dataclasses import (
 )
 from typing import Optional
 
-from landoapi.systems import Subsystem
+from lando.api.legacy.systems import Subsystem
 
 logger = logging.getLogger(__name__)
 
--- src/lando/ui/legacy/revisions.py
+++ src/lando/ui/legacy/revisions.py
@@ -8,6 +8,8 @@ from lando.ui.legacy.forms import (
 from lando.ui.legacy.stacks import draw_stack_graph, Edge, sort_stack_topological
 from lando.ui.views import LandoView
 
+from lando.api.legacy import api as legacy_api
+
 from django.template.response import TemplateResponse
 from django.http import JsonResponse, HttpResponseNotFound
 
@@ -21,135 +23,121 @@ class Revision(LandoView):
     # TODO: auth is optional for this view.
 
     def get(self, request, revision_id, *args, **kwargs):
-        # TODO: this should be implemented once this is merged with lando-api.
-        return TemplateResponse(template_name="stack/stack.html")
+        stack = legacy_api.stacks.get(revision_id)
 
-        # # Request the entire stack.
-        # try:
-        #     stack = api.request("GET", "stacks/D{}".format(revision_id))
-        # except LandoAPIError as e:
-        #     if e.status_code == 404:
-        #         raise RevisionNotFound(revision_id)
-        #     else:
-        #         raise
+        # Build a mapping from phid to revision and identify
+        # the data for the revision used to load this page.
+        revision = None
+        revisions = {}
+        for r in stack["revisions"]:
+            revisions[r["phid"]] = r
+            if r["id"] == "D{}".format(revision_id):
+                revision = r["phid"]
 
-        # # Build a mapping from phid to revision and identify
-        # # the data for the revision used to load this page.
-        # revision = None
-        # revisions = {}
-        # for r in stack["revisions"]:
-        #     revisions[r["phid"]] = r
-        #     if r["id"] == "D{}".format(revision_id):
-        #         revision = r["phid"]
+        # Build a mapping from phid to repository.
+        repositories = {}
+        for r in stack["repositories"]:
+            repositories[r["phid"]] = r
 
-        # # Build a mapping from phid to repository.
-        # repositories = {}
-        # for r in stack["repositories"]:
-        #     repositories[r["phid"]] = r
+        # Request all previous transplants for the stack.
+        transplants = legacy_api.transplants.get_list(revision_id)
 
-        # # Request all previous transplants for the stack.
-        # transplants = api.request(
-        #     "GET", "transplants", params={"stack_revision_id": "D{}".format(revision_id)}
-        # )
+        # The revision may appear in many `landable_paths`` if it has
+        # multiple children, or any of its landable descendents have
+        # multiple children. That being said, there should only be a
+        # single unique path up to this revision, so find the first
+        # it appears in. The revisions up to the target one in this
+        # path form the landable series.
+        #
+        # We also form a set of all the revisions that are landable
+        # so we can present selection for what to land.
+        series = None
+        landable = set()
+        for p in stack["landable_paths"]:
+            for phid in p:
+                landable.add(phid)
 
-        # # The revision may appear in many `landable_paths`` if it has
-        # # multiple children, or any of its landable descendents have
-        # # multiple children. That being said, there should only be a
-        # # single unique path up to this revision, so find the first
-        # # it appears in. The revisions up to the target one in this
-        # # path form the landable series.
-        # #
-        # # We also form a set of all the revisions that are landable
-        # # so we can present selection for what to land.
-        # series = None
-        # landable = set()
-        # for p in stack["landable_paths"]:
-        #     for phid in p:
-        #         landable.add(phid)
+            try:
+                series = p[: p.index(revision) + 1]
+            except ValueError:
+                pass
 
-        #     try:
-        #         series = p[: p.index(revision) + 1]
-        #     except ValueError:
-        #         pass
+        dryrun = None
+        target_repo = None
+        if series and is_user_authenticated():
+            landing_path = [
+                {
+                    "revision_id": revisions[phid]["id"],
+                    "diff_id": revisions[phid]["diff"]["id"],
+                }
+                for phid in series
+            ]
+            landing_path_json = json.dumps(landing_path)
+            form.landing_path.data = landing_path_json
 
-        # dryrun = None
-        # target_repo = None
-        # if series and is_user_authenticated():
-        #     landing_path = [
-        #         {
-        #             "revision_id": revisions[phid]["id"],
-        #             "diff_id": revisions[phid]["diff"]["id"],
-        #         }
-        #         for phid in series
-        #     ]
-        #     landing_path_json = json.dumps(landing_path)
-        #     form.landing_path.data = landing_path_json
+            dryrun = legacy_api.transplants.dry_run(data={"landing_path": landing_path})
+            series = list(reversed(series))
+            target_repo = repositories.get(revisions[series[0]]["repo_phid"])
 
-        #     dryrun = api.request(
-        #         "POST",
-        #         "transplants/dryrun",
-        #         require_auth0=True,
-        #         json={"landing_path": landing_path},
-        #     )
-        #     form.confirmation_token.data = dryrun.get("confirmation_token")
+        phids = set(revisions.keys())
+        edges = set(Edge(child=e[0], parent=e[1]) for e in stack["edges"])
+        order = sort_stack_topological(
+            phids, edges, key=lambda x: int(revisions[x]["id"][1:])
+        )
+        drawing_width, drawing_rows = draw_stack_graph(phids, edges, order)
 
-        #     series = list(reversed(series))
-        #     target_repo = repositories.get(revisions[series[0]]["repo_phid"])
+        annotate_sec_approval_workflow_info(revisions)
 
-        # phids = set(revisions.keys())
-        # edges = set(Edge(child=e[0], parent=e[1]) for e in stack["edges"])
-        # order = sort_stack_topological(
-        #     phids, edges, key=lambda x: int(revisions[x]["id"][1:])
-        # )
-        # drawing_width, drawing_rows = draw_stack_graph(phids, edges, order)
+        # Are we showing the "sec-approval request submitted" dialog?
+        # If we are then fill in its values.
+        submitted_revision = request.args.get("show_approval_success")
+        submitted_rev_url = None
+        if submitted_revision:
+            for rev in revisions.values():
+                if rev["id"] == submitted_revision:
+                    submitted_rev_url = rev["url"]
+                    break
 
-        # annotate_sec_approval_workflow_info(revisions)
+        # Current implementation requires that all commits have the flags appended.
+        # This may change in the future. What we do here is:
+        # - if all commits have the flag, then disable the checkbox
+        # - if any commits do not have the flag, then enable the checkbox
 
-        # # Are we showing the "sec-approval request submitted" dialog?
-        # # If we are then fill in its values.
-        # submitted_revision = request.args.get("show_approval_success")
-        # submitted_rev_url = None
-        # if submitted_revision:
-        #     for rev in revisions.values():
-        #         if rev["id"] == submitted_revision:
-        #             submitted_rev_url = rev["url"]
-        #             break
+        if target_repo:
+            existing_flags = {f[0]: False for f in target_repo["commit_flags"]}
+            for flag in existing_flags:
+                existing_flags[flag] = all(
+                    flag in r["commit_message"] for r in revisions.values()
+                )
+        else:
+            existing_flags = {}
 
-        # # Current implementation requires that all commits have the flags appended.
-        # # This may change in the future. What we do here is:
-        # # - if all commits have the flag, then disable the checkbox
-        # # - if any commits do not have the flag, then enable the checkbox
+        context = {
+            "revision_id": "D{}".format(revision_id),
+            "series": series,
+            "landable": landable,
+            "dryrun": dryrun,
+            "stack": stack,
+            "rows": list(zip(reversed(order), reversed(drawing_rows))),
+            "drawing_width": drawing_width,
+            "transplants": transplants,
+            "revisions": revisions,
+            "revision_phid": revision,
+            "sec_approval_form": sec_approval_form,
+            "submitted_rev_url": submitted_rev_url,
+            "target_repo": target_repo,
+            "errors": errors,
+            "form": form,
+            "flags": target_repo["commit_flags"] if target_repo else [],
+            "existing_flags": existing_flags,
+            "uplift_request_form": uplift_request_form,
+        }
 
-        # if target_repo:
-        #     existing_flags = {f[0]: False for f in target_repo["commit_flags"]}
-        #     for flag in existing_flags:
-        #         existing_flags[flag] = all(
-        #             flag in r["commit_message"] for r in revisions.values()
-        #         )
-        # else:
-        #     existing_flags = {}
-
-        # return render_template(
-        #     "stack/stack.html",
-        #     revision_id="D{}".format(revision_id),
-        #     series=series,
-        #     landable=landable,
-        #     dryrun=dryrun,
-        #     stack=stack,
-        #     rows=list(zip(reversed(order), reversed(drawing_rows))),
-        #     drawing_width=drawing_width,
-        #     transplants=transplants,
-        #     revisions=revisions,
-        #     revision_phid=revision,
-        #     sec_approval_form=sec_approval_form,
-        #     submitted_rev_url=submitted_rev_url,
-        #     target_repo=target_repo,
-        #     errors=errors,
-        #     form=form,
-        #     flags=target_repo["commit_flags"] if target_repo else [],
-        #     existing_flags=existing_flags,
-        #     uplift_request_form=uplift_request_form,
-        # )
+        return TemplateResponse(
+            request=request,
+            template="stack/stack.html",
+            context=context,
+        )
 
     def post(*args, request, **kwargs):
         # TODO: this should be implemented once this is merged with lando-api.
